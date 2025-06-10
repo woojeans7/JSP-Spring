d18cd34db4483d81dd75e977ed69c10eb69eaf3a
woojeans7 <thddnwls71@naver.com>
2025-06-10 15:35:51 +0900

    Fix : 01 기본문제 오류 해결 06-10

--- Spring/01_spring(basic)/ex01/src/main/java/org/scoula/app/Main.java
+++ Spring/01_spring(basic)/ex01/src/main/java/org/scoula/app/Main.java
@@ -15,6 +15,5 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
-
     }
 }
