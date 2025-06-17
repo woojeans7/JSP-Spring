694e2e6d4fd6e111fa1287bd43b3163a53210739
woojeans7 <thddnwls71@naver.com>
2025-06-17 15:39:34 +0900

    Fix : 문제풀이 추가 06-17

---
+++ Spring/04_spring(advanced)/board/src/main/java/org/scoula/board/controller/BoardController.java
@@ -0,0 +1,61 @@
+package org.scoula.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.board.dto.BoardDTO;
+import org.scoula.board.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Log4j2
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    final private BoardService service;
+
+    @GetMapping("/list")
+    public void list(Model model) {
+
+        log.info("list");
+        model.addAttribute("list", service.getList());
+    }
+
+    @GetMapping("/create")
+    public void create() {
+        log.info("create");
+    }
+
+    @PostMapping("/create")
+    public String create(BoardDTO board) {
+        log.info("create");
+
+        service.create(board);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping({"/get", "/update"})
+    public void get(@RequestParam("no") Long no, Model model) {
+        log.info("get or update");
+        model.addAttribute("board", service.get(no));
+    }
+
+    @PostMapping("/update")
+    public String update(BoardDTO board) {
+        log.info("update:" + board);
+        service.update(board);
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam("no") Long no) {
+        log.info("delete..." + no);
+        service.delete(no);
+        return "redirect:/board/list";
+    }
+}
--- Spring/04_spring(advanced)/board/src/main/java/org/scoula/config/ServletConfig.java
+++ Spring/04_spring(advanced)/board/src/main/java/org/scoula/config/ServletConfig.java
@@ -15,7 +15,8 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = {
         "org.scoula.exception",
-        "org.scoula.controller"
+        "org.scoula.controller",
+        "org.scoula.board.controller"
 })
 public class ServletConfig implements WebMvcConfigurer {
 
