0b6e10a1a59a5af043aca2f2d528b95966198f96
woojeans7 <thddnwls71@naver.com>
2025-06-23 23:54:16 +0900

    Solve : 09 기본문제 풀이 06-23

---
+++ Spring/09_spring(basic)/secserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package org.scoula.security.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 문자셋 필터
+    public CharacterEncodingFilter encodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
+
+        // 경로별 접근 권한 설정
+        http.authorizeRequests()
+                .antMatchers("/security/all").permitAll()
+                .antMatchers("/security/admin").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/security/member").access("hasAnyRole('ROLE_MEMBER', 'ROLE_ADMIN')");
+
+        http.formLogin()
+                .loginPage("/security/login")
+                .loginProcessingUrl("/security/login")
+                .defaultSuccessUrl("/");
+
+        http.logout() // 로그아웃 설정 시작
+                .logoutUrl("/security/logout") // POST: 로그아웃 호출 url
+                .invalidateHttpSession(true) // 세션 invalidate
+                .deleteCookies("remember-me", "JSESSION-ID") // 삭제할 쿠키 목록
+                .logoutSuccessUrl("/security/logout"); // GET: 로그아웃 이후 이동할 페이지
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        log.info("configure .........................................");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+//                .password("{noop}1234")
+                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
+                .roles("ADMIN", "MEMBER"); // ROLE_ADMIN
+        auth.inMemoryAuthentication()
+                .withUser("member")
+//                .password("{noop}1234")
+                .password("$2a$10$EsIMfxbJ6NuvwX7MDj4WqOYFzLU9U/lddCyn0nic5dFo3VfJYrXYC")
+                .roles("MEMBER"); // ROLE_MEMBER
+    }
+}
---
+++ Spring/09_spring(basic)/secserver/src/main/webapp/WEB-INF/views/security/all.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>/security/all page</h1>
+</body>
+</html>
